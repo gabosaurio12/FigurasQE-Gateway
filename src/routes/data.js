87a468125d112dff7a752b839eb7b0752217f173
Gabo-Mac <gabgonlo05@gmail.com>
2026-05-09 17:07:54 -0600

    data feat

--- src/routes/data.js
+++ src/routes/data.js
@@ -31,4 +31,81 @@ router.get('/students/:id', async (req, res) => {
     }
 });
 
+router.put('/students/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const response = await axios.put(
+            `${DATA_SERVICE}/students/${id}`,
+            req.body,
+            {
+                headers: {
+                    Authorization: req.headers.authorization,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("=== REAL ERROR FROM DATA SERVICE ===");
+
+        console.error("STATUS:", error.response?.status);
+        console.error("DATA:", error.response?.data);
+        console.error("MESSAGE:", error.message);
+
+        res.status(500).json({
+            message: "Data Service Error",
+            details: error.response?.data || error.message
+        });
+    }
+});
+
+router.get('/tutors/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const response = await axios.get(
+            `${DATA_SERVICE}/tutors/${id}`,
+            {
+                headers: {
+                    Authorization: req.headers.authorization
+                }
+            }
+        );
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: "Data Service Error" });
+    }
+});
+
+router.put('/tutors/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const response = await axios.put(
+            `${DATA_SERVICE}/tutors/${id}`,
+            req.body,
+            {
+                headers: {
+                    Authorization: req.headers.authorization,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+        res.json(response.data);
+    } catch (error) {
+        console.error("=== REAL ERROR FROM DATA SERVICE ===");
+
+        console.error("STATUS:", error.response?.status);
+        console.error("DATA:", error.response?.data);
+        console.error("MESSAGE:", error.message);
+
+        res.status(500).json({
+            message: "Data Service Error",
+            details: error.response?.data || error.message
+        });
+    }
+});
+
 module.exports = router;
